c678f68f7faaa247c067ee03acbe696c6a8ac5f8
Zair <nurmukhamadov.z@future-group.ru>
2021-11-07 11:56:24 +0300

    finish

--- config/lara-api-auth.php
+++ config/lara-api-auth.php
@@ -4,4 +4,17 @@ return [
 	'access_token_lifetime' => env('ACCESS_TOKEN_LIFETIME', 86400),
 	'refresh_token_lifetime' => env('REFRESH_TOKEN_LIFETIME', 86400 * 7),
 	'refresh_token_cache_domain' => env('REFRESH_TOKEN_COOKIE_DOMAIN', 'http://localhost'),
+
+	/**
+	 * Не нужные роуты закомментируйте
+	 */
+	'routes' => [
+		'laraApiAuth.signUp', // Route: /api/auth/sign-up
+		'laraApiAuth.signIn', // Route: /api/auth/sign-in
+		'laraApiAuth.refreshToken', // Route: /api/auth/refresh-token
+		'laraApiAuth.signOut', // Route: /api/auth/sign-out
+		'laraApiAuth.me', // Route: /api/me
+		'password.email', // Route: /api/password/email
+		'password.reset', // Route: /api/password/reset
+	],
 ];
--- routes/api.php
+++ routes/api.php
@@ -10,21 +10,37 @@ use Future\LaraApiAuth\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'api'], function () {
+	$routeConfigs = config('lara-api-auth.routes');
 
-	Route::group(['prefix' => 'auth'], function () {
-		Route::post('sign-up', [SignUpController::class, 'signUp'])->name('laraApiAuth.signUp');
+	Route::group(['prefix' => 'auth'], function () use ($routeConfigs) {
+		if (in_array('laraApiAuth.signUp', $routeConfigs, true)) {
+			Route::post('sign-up', [SignUpController::class, 'signUp'])->name('laraApiAuth.signUp');
+		}
 
-		Route::post('sign-in', [SignInController::class, 'signIn'])->name('laraApiAuth.signIn');
-		Route::post('refresh-token', [RefreshController::class, 'refreshToken'])->name('laraApiAuth.refreshToken');
-		Route::get('sign-out', [SignOutController::class, 'signOut'])->middleware('auth:api')->name('laraApiAuth.signOut');
+		if (in_array('laraApiAuth.signIn', $routeConfigs, true)) {
+			Route::post('sign-in', [SignInController::class, 'signIn'])->name('laraApiAuth.signIn');
+		}
 
-		// Route::get('verify-email', [VerifyController::class, 'verify'])->name('laraApiAuth.verify');
+		if (in_array('laraApiAuth.refreshToken', $routeConfigs, true)) {
+			Route::post('refresh-token', [RefreshController::class, 'refreshToken'])->name('laraApiAuth.refreshToken');
+		}
+
+		if (in_array('laraApiAuth.signOut', $routeConfigs, true)) {
+			Route::get('sign-out', [SignOutController::class, 'signOut'])->middleware('auth:api')->name('laraApiAuth.signOut');
+		}
 	});
 
-	Route::get('/me', [UserController::class, 'user'])->middleware('auth:api')->name('laraApiAuth.me');
+	if (in_array('laraApiAuth.me', $routeConfigs, true)) {
+		Route::get('/me', [UserController::class, 'user'])->middleware('auth:api')->name('laraApiAuth.me');
+	}
 
-	Route::group(['prefix' => 'password'], function () {
-		Route::post('email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-		Route::post('reset', [ResetPasswordController::class, 'reset'])->name('password.reset');
+	Route::group(['prefix' => 'password'], function () use ($routeConfigs) {
+		if (in_array('password.email', $routeConfigs, true)) {
+			Route::post('email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+		}
+
+		if (in_array('password.reset', $routeConfigs, true)) {
+			Route::post('reset', [ResetPasswordController::class, 'reset'])->name('password.reset');
+		}
 	});
 });
